884c6d6fc49f4d65ca111a886aca580778cc202a
Gawah Chan <gawahchan@gmail.com>
2024-10-18 14:48:46 +0100

    Add DeleteTask Functionality

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -4,25 +4,30 @@ class TaskTile extends StatelessWidget {
   final String taskTitle;
   final bool isChecked;
   final ValueChanged<bool?> checkboxCallback;
+  final GestureLongPressCallback longPressCallback;
 
-  const TaskTile(
-      {super.key,
-      required this.taskTitle,
-      required this.isChecked,
-      required this.checkboxCallback});
+  const TaskTile({
+    super.key,
+    required this.taskTitle,
+    required this.isChecked,
+    required this.checkboxCallback,
+    required this.longPressCallback,
+  });
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-        title: Text(
-          taskTitle,
-          style: TextStyle(
-              decoration: isChecked ? TextDecoration.lineThrough : null),
-        ),
-        trailing: Checkbox(
-          activeColor: Colors.lightBlueAccent,
-          value: isChecked,
-          onChanged: checkboxCallback,
-        ));
+      title: Text(
+        taskTitle,
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
+      ),
+      trailing: Checkbox(
+        activeColor: Colors.lightBlueAccent,
+        value: isChecked,
+        onChanged: checkboxCallback,
+      ),
+      onLongPress: longPressCallback,
+    );
   }
 }
